ddb7261c4d4fdaf27b72b53efd8d6afdd3899f29
MatteoMartinelliMM <matteomartinelli1b@gmail.com>
2025-03-29 11:23:05 +0100

    Implemented homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,7 @@
 import Navbar from "@/app/components/navbar";
-
+import Landing from "@/app/components/landing";
+import ParticlesBackground from "@/app/components/landingBackground";
+import Experience from "@/app/components/experience";
 
 export default function Home() {
     return (<div className="flex flex-col items-center justify-center">
@@ -8,13 +10,13 @@ export default function Home() {
         <section id="home" className="w-screen h-screen ">
 
             <div className=" relative w-screen h-[93%] flex items-center justify-center z-10">
-                <h1>HOME</h1>
+                <Landing></Landing>
             </div>
         </section>
         <div className="flex-grow main-body">
             <section id="experience"
                      className="min-h-screen flex">
-                <h1>EXPERIENCE</h1></section>
+                    <Experience></Experience></section>
                 <section id="knowledge" className="min-h-screen flex items-center justify-center  ">
                     <h1>KNOWLEDGE</h1></section>
                 <section id="projects"
