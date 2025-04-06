2729543be90f0990ff8c0c48186f6ce58bd813b9
MatteoMartinelliMM <matteomartinelli1b@gmail.com>
2025-04-06 17:53:53 +0200

    DEV: add numeric values to progress REFACTOR: refactor knowledge component and add language to model

--- src/app/components/knowledge/knowledge.jsx
+++ src/app/components/knowledge/knowledge.jsx
@@ -7,59 +7,54 @@ import AnimatedProgress from "@/app/components/clientComponents/animatedProgress
 const knowledgeMap = init()
 
 function Knowledge() {
+    console.log(knowledgeMap.get(KnowledgeType.LANGUAGE))
     return <div className="container mx-auto">
         <div className="grid grid-cols-1 gap-4 lg:grid-cols-2 lg:gap-12">
             {
-                Array.from(knowledgeMap.keys()).map(k =>
-                    <div key={k.label()} className="flex flex-col ">
-                        <div key={k.dividerStyle()} className={`divider divider-start ${k.dividerStyle()}`}><h2
-                            key={k.label()}
-                            className="text-2xl">{k.label()}</h2>
-                        </div>
-                        {
-                            knowledgeMap.get(k).map(v =>
-                                <div key={`${k.label()}-${v.label}`}
-                                     className="flex justify-between items-center gap-8 w-full">
-                                    <FadeComponent><span className="w-1/4 text-left ">{`${v.label.toUpperCase()}:`}</span></FadeComponent>
-                                    <AnimatedProgress value={v.value}>
-                                        <progress className={`progress w-3/5 md:w-3/4 ${k.progressStyle()}`} value={v.value}
-                                                  max="10"/>
-                                    </AnimatedProgress>
-                                </div>
-                            )
-                        }
-                    </div>
-                )
+                buildKnowledgeByType(false)
             }
         </div>
         <div className="container mx-auto flex flex-col mt-5">
-            <div className="divider divider-start divider-success"><h2 className="text-2xl">SPOKEN LANGUAGES</h2></div>
-            <div className="flex justify-between items-center gap-8 w-full">
-                <FadeComponent><span className="w-1/6 text-left">ITALIAN:</span></FadeComponent>
-                <AnimatedProgress value="10">
-                    <progress className={`progress  progress-success w-4/6 md:w-5/6`} value={10}
-                              max="10"/>
-                </AnimatedProgress>
-            </div>
-            <div className="flex justify-between items-center gap-8 w-full">
-                <FadeComponent><span className="w-1/6 text-left">ENGLISH:</span></FadeComponent>
-                <AnimatedProgress value="9">
-                    <progress className="progress progress-success w-4/6 md:w-5/6" value={9}
-                              max="10"/>
-                </AnimatedProgress>
-            </div>
-            <div className="flex justify-between items-center gap-8 w-full">
-                <FadeComponent><span className="w-1/6 text-left">GERMAN:</span></FadeComponent>
-                <AnimatedProgress value="8">
-                    <progress className="progress progress-success w-4/6 md:w-5/6" value={9}
-                              max="10"/>
-                </AnimatedProgress>
-            </div>
+            {
+                buildKnowledgeByType(true)
+            }
         </div>
     </div>
 
 }
-
+function buildKnowledgeByType(isLanguages= false){
+    const knowledgeEntriesByType = !isLanguages ? Array.from(knowledgeMap.keys()).filter(k => k !== KnowledgeType.LANGUAGE) :
+        Array.from(knowledgeMap.keys()).filter(k => k === KnowledgeType.LANGUAGE)
+    return knowledgeEntriesByType.map(k =>
+        <div key={k.label()} className="flex flex-col w-full">
+            <div key={k.dividerStyle()} className={`divider divider-start ${k.dividerStyle()}`}>
+                <h2 key={k.label()} className="text-2xl">{k.label()}</h2>
+            </div>
+            {
+                knowledgeMap.get(k).map(v =>
+                    <div key={`${k.label()}-${v.label}`} className="flex items-center w-full">
+                        <div className={`${isLanguages ? 'w-1/12' :'w-1/4'} flex flex-shrink-0 items-start`}>
+                            <FadeComponent>
+                                <span className="text-left font-medium">{`${v.label.toUpperCase()}:`}</span>
+                            </FadeComponent>
+                        </div>
+                        <div className={`${isLanguages ? 'w-11/12' :'w-3/4'} flex justify-end items-center gap-4 `}>
+                            <div className="flex-grow mr-4">
+                                <AnimatedProgress value={v.value}>
+                                    <progress className={`progress w-full ${k.progressStyle()} `}
+                                              value={v.value} max="10"/>
+                                </AnimatedProgress>
+                            </div>
+                            <FadeComponent>
+                                <span className="inline-block w-12 text-right">{`${v.value}/10`}</span>
+                            </FadeComponent>
+                        </div>
+                    </div>
+                )
+            }
+        </div>
+    )
+}
 function init() {
     const knowledgeMap = new Map();
     knowledgeMap.set(KnowledgeType.MOBILE, [
@@ -70,17 +65,23 @@ function init() {
     knowledgeMap.set(KnowledgeType.WEB, [
         new KnowledgeEntry('Javascript', KnowledgeType.WEB, 9),
         new KnowledgeEntry('React', KnowledgeType.WEB, 7),
-        new KnowledgeEntry('NextJS', KnowledgeType.WEB, 7),
+        new KnowledgeEntry('Next.JS', KnowledgeType.WEB, 8),
         new KnowledgeEntry('Tailwind', KnowledgeType.WEB, 7),
     ])
     knowledgeMap.set(KnowledgeType.DB_BACKEND, [
         new KnowledgeEntry('PosgreSQL', KnowledgeType.DB_BACKEND, 9),
+        new KnowledgeEntry('Node.js', KnowledgeType.DB_BACKEND, 9),
         new KnowledgeEntry('Firebase', KnowledgeType.DB_BACKEND, 10),
         new KnowledgeEntry('Supabase', KnowledgeType.DB_BACKEND, 8),
-        new KnowledgeEntry('Mongodb', KnowledgeType.DB_BACKEND, 7),
+        new KnowledgeEntry('MongoDB', KnowledgeType.DB_BACKEND, 7),
     ])
     knowledgeMap.set(KnowledgeType.MD_PROCESS, [
         new KnowledgeEntry('FFmpeg', KnowledgeType.MD_PROCESS, 6),
+    ])
+    knowledgeMap.set(KnowledgeType.LANGUAGE, [
+        new KnowledgeEntry('Italian', KnowledgeType.LANGUAGE, 10),
+        new KnowledgeEntry('English', KnowledgeType.LANGUAGE, 9),
+        new KnowledgeEntry('German', KnowledgeType.LANGUAGE, 8),
     ])
     return knowledgeMap
 }
--- src/model/knowledgeType.js
+++ src/model/knowledgeType.js
@@ -7,6 +7,7 @@ class KnowledgeType {
     static WEB = new KnowledgeType('web');
     static DB_BACKEND = new KnowledgeType('db & backend');
     static MD_PROCESS = new KnowledgeType('media proc.');
+    static LANGUAGE = new KnowledgeType('language');
 
 
     dividerStyle() {
@@ -20,10 +21,12 @@ class KnowledgeType {
                 return 'divider-db'
             case 'media proc.':
                 return 'divider-media'
+            case 'language':
+                return 'divider-languages'
         }
     }
 
-    progressStyle(){
+    progressStyle() {
         switch (this.type) {
             case 'mobile':
                 return 'progress-mobile';
@@ -33,6 +36,8 @@ class KnowledgeType {
                 return 'progress-db';
             case 'media proc.':
                 return 'progress-media';
+            case 'language':
+                return 'progress-languages';
         }
     }
 
@@ -41,7 +46,7 @@ class KnowledgeType {
     }
 
     static values() {
-        return [this.MOBILE, this.WEB, this.DB_BACKEND, this.MD_PROCESS];
+        return [this.MOBILE, this.WEB, this.DB_BACKEND, this.MD_PROCESS, this.LANGUAGE];
     }
 }
 
