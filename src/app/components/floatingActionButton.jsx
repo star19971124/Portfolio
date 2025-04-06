4c5eb6ccb64e344a46cf3efb5e67d6f8a887f9f7
MatteoMartinelliMM <matteomartinelli1b@gmail.com>
2025-04-06 19:30:22 +0200

    DEV: change floating action button transition duration

--- src/app/components/floatingActionButton.jsx
+++ src/app/components/floatingActionButton.jsx
@@ -3,16 +3,16 @@
 import {useEffect, useState} from "react";
 
 function FloatingActionButton() {
-    const [isButtonVisible, setIsButtonVisibile] = useState(false)
+    const [isButtonVisible, setIsButtonVisible] = useState(false)
     useEffect(() => {
 
         window.addEventListener('scroll', () => {
             const homeSection = document.getElementById("home")
             const homeHeight = homeSection ? homeSection.offsetHeight : 0
-            setIsButtonVisibile(window.scrollY > homeHeight * 0.35)
+            setIsButtonVisible(window.scrollY > homeHeight * 0.35)
         })
     })
-    return <div className={`fixed bottom-0 right-0 mb-6 mr-6 transition-all duration-150  ${isButtonVisible ? 'opacity-100 scale-100': 'opacity-0 scale-0 pointer-events-none'}`}>
+    return <div className={`fixed bottom-0 right-0 mb-6 mr-6 transition-all duration-300  ${isButtonVisible ? 'opacity-100 scale-100': 'opacity-0 scale-0 pointer-events-none'}`}>
         <a href="#home">
             <button className="btn btn-circle btn-outline">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
