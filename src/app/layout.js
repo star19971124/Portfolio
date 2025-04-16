78cd9fa6f8d62d456a3cb0839286e668839a1746
MatteoMartinelliMM <matteomartinelli1b@gmail.com>
2025-04-16 19:40:14 +0200

    DEV: add vercel analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,14 @@
 import './global.css';
+import {Analytics} from "@vercel/analytics/react"
+
 
 export default function RootLayout({children, params}) {
     return (
         <html lang={params.locale}>
         <head>
-            <link rel="icon" href="/logo_short.svg" />
+            <link rel="icon" href="/logo_short.svg"/>
         </head>
-        <body className="antialiased">{children}</body>
+        <body className="antialiased"><Analytics>{children}</Analytics></body>
         </html>
     );
 }
